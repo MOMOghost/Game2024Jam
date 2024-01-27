d7003f397054d6c4c985169d7b72efce2164779c
MOMOghost <89346156+MOMOghost@users.noreply.github.com>
2024-01-27 10:58:54 +0800

    feat: 水平移動

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 1;
+    private Vector2 movement;
+
+    private Rigidbody2D _rigidbody2D;
+    private BoxCollider2D _boxCollider2D;
+    private Animator _animator;
+
     void Start()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+        _animator = GetComponent<Animator>();
+
+         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if(movement!=null)
+        // 在Update中调用Move
+        Move();
+    }
+
+    public void MoveToLeft()
+    {
+        movement.x = -1;
+        Debug.Log("MoveToLeft" + movement.x);
+    }
+
+    public void MoveToRight()
+    {
+        movement.x = 1;
+        Debug.Log("MoveToRight" + movement.x);
+    }
+
+    private void Move()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal"); 
+        // Debug.Log("Move");
+        _rigidbody2D.velocity = new Vector2(movement.x * speed, _rigidbody2D.velocity.y);
     }
 }
