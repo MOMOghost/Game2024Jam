1ca904dffccecd19ac4630a7b1fcf7dd1228ac8d
bootcat123shecat123 <oooth16@gmail.com>
2024-01-27 14:31:08 +0800

    feat:blockSpawn

---
+++ Assets/Area/BlockSpawn.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawn : MonoBehaviour
+{
+    BoxCollider2D areaBound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
