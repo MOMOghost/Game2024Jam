cb4e6398a9e5b84aba7cd8d878b44f518a1c5a21
MOMOghost <89346156+MOMOghost@users.noreply.github.com>
2024-01-28 14:27:18 +0800

    feat: vu歐正

--- Assets/梗圖觸發.cs
+++ Assets/梗圖觸發.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using SupSystem;
+using System.Diagnostics;
 
 public class 梗圖觸發 : MonoBehaviour
 {
@@ -34,6 +35,56 @@ public class 梗圖觸發 : MonoBehaviour
         int randomInt = random.Next(0, 16);
 
         梗圖.GetComponent<SpriteRenderer>().sprite = 梗圖清單[randomInt];
+
+
+
+        switch(randomInt) {
+
+            case 2:
+            case 15:
+                 soundController.PlayAudio(soundController.SE[0], SoundController.AudioType.SE, false);
+                break;
+
+            case 4:
+             case 5:
+                 case 7:
+             case 10:
+                case 13:
+                 case 14:
+             case 16:
+                 soundController.PlayAudio(soundController.SE[2], SoundController.AudioType.SE, false);
+                break;
+
+            case 3:
+                 soundController.PlayAudio(soundController.SE[1], SoundController.AudioType.SE, false);
+                break;
+                  case 6:
+              soundController.PlayAudio(soundController.SE[3], SoundController.AudioType.SE, false);
+                break;
+                        case 8:
+                 soundController.PlayAudio(soundController.SE[4], SoundController.AudioType.SE, false);
+                break;
+
+                              case 9:
+                 soundController.PlayAudio(soundController.SE[5], SoundController.AudioType.SE, false);
+                break;
+                                   case 11:
+                 soundController.PlayAudio(soundController.SE[6], SoundController.AudioType.SE, false);
+                break;
+                                   case 12:
+                 soundController.PlayAudio(soundController.SE[7], SoundController.AudioType.SE, false);
+                break;
+                                   case 17:
+                soundController.PlayAudio(soundController.SE[8], SoundController.AudioType.SE, false);
+                break;
+                                   case 18:
+                soundController.PlayAudio(soundController.SE[9], SoundController.AudioType.SE, false);
+                break;
+
+            default:
+                soundController.PlayAudio(soundController.SE[9], SoundController.AudioType.SE, false);
+                break;
+        }
         //soundController.PlayAudio(soundController.BGM[randomInt], SoundController.AudioType.BGM, true);
 
     }
