20415d4099934bcdbb3f98dd87fe03e4807fa747
MOMOghost <89346156+MOMOghost@users.noreply.github.com>
2024-01-28 11:07:12 +0800

    feat: 梗圖觸發製作中

---
+++ Assets/梗圖觸發.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 梗圖觸發 : MonoBehaviour
+{
+    public List<Sprite> 梗圖清單;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
